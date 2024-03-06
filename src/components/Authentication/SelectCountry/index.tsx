0176e3256f29115fdc29a61136887c603acb88ac
Udit tyagi <udittyagi455@gmail.com>
2024-03-06 22:35:43 +0530

    work on select country component

--- src/components/Authentication/SelectCountry/index.tsx
+++ src/components/Authentication/SelectCountry/index.tsx
@@ -1,84 +1,135 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react';
-import CountryPicker, { DARK_THEME } from 'react-native-country-picker-modal'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { style } from "./style";
-import Icon from "react-native-vector-icons/Ionicons"
-import { useNavigation } from '@react-navigation/native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import CountryPicker, {DARK_THEME} from 'react-native-country-picker-modal';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {style} from './style';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 const SelectCountry = () => {
-    const [countryName, setCountryName] = useState("")
-    const [withCallingCode, setWithCallingCode] = useState(true)
-    const [withAlphaFilter, setWithAlphaFilter] = useState(true)
-    const [withFilter, setwithFilter] = useState(true)
-    const [countryCode, setCountryCode] = useState('US');
-    const [callingCode, setCallingCode] = useState("")
-    const [visible, setVisible] = useState(false);
+  const [countryName, setCountryName] = useState('');
+  const [withCallingCode, setWithCallingCode] = useState(true);
+  const [withAlphaFilter, setWithAlphaFilter] = useState(true);
+  const [withFilter, setwithFilter] = useState(true);
+  const [countryCode, setCountryCode] = useState('US');
+  const [callingCode, setCallingCode] = useState('');
+  const [visible, setVisible] = useState(false);
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const countryChange = ({ flag }) => {
-        console.log("flag ::", flag);
-        // setState(state => ({
-        //     ...state,
-        //     flag: flag,
-        // }));
-    };
+  const countryChange = ({flag}) => {
+    console.log('flag ::', flag);
+    // setState(state => ({
+    //     ...state,
+    //     flag: flag,
+    // }));
+  };
 
-    const SelectCountry = (e) => {
-        console.log("country-select ::", e);
-        setCountryName(e.name);
-        setCallingCode(e.callingCode[0]);
-    };
-    const nextPress = () => {
-        navigation.navigate("selectTopics");
-    }
-    return (
-        <KeyboardAwareScrollView style={{ backgroundColor: "black", height: hp("100%") }}>
-            <View style={{ display: "flex", flexDirection: "row", alignItems: "center", marginVertical: 10 }}>
-                <Icon name="arrow-back-outline" size={30} color="white" onPress={() => navigation.pop()} />
-                <Text style={{ color: "white", marginLeft: 100, fontWeight: "600", fontSize: 16 }}>Select your country</Text>
-            </View>
-            <View style={{ display: "flex", justifyContent: "space-between", flexDirection: "column" }}>
-                <View style={{ marginTop: 40, marginHorizontal: 10 }}>
-                    <TextInput
-                        value={`${callingCode} - ${countryName}`}
-                        placeholder='select country...'
-                        placeholderTextColor={"white"}
-                        style={{
-                            paddingVertical: 15,
-                            backgroundColor: "#3A3B3C",
-                            color: "white",
-                            fontSize: 16,
-                            fontWeight: "600",
-                            borderRadius: 8
-                        }} />
-                    <Icon name="caret-down-outline" size={35} color="white" style={{ position: "absolute", right: 10, top: 12 }} onPress={() => setVisible(true)} />
-                    <CountryPicker
-                        {...{
-                            withCallingCode,
-                            withAlphaFilter,
-                            withFilter,
-                            countryChange,
-                        }}
-                        onSelect={(country) => SelectCountry(country)}
-                        modalProps={{
-                            visible: visible
-                        }}
-                        onOpen={() => setVisible(true)}
-                        onClose={() => setVisible(false)} />
-                </View>
-                <TouchableOpacity style={{ backgroundColor: "blue", paddingVertical: 15, alignItems: "center", marginHorizontal: 10, borderRadius: 8, marginTop: hp("68%") }} onPress={nextPress}>
-                    <Text style={{ color: "white", fontSize: 18 }}>Next</Text>
-                </TouchableOpacity>
-            </View>
-
-
-        </KeyboardAwareScrollView>
-    )
-}
+  const SelectCountry = e => {
+    setCountryName(e.name);
+    setCallingCode(e.callingCode[0]);
+  };
+  const nextPress = () => {
+    navigation.navigate('selectTopics');
+  };
+  return (
+    <KeyboardAwareScrollView
+      style={{backgroundColor: 'black', height: hp('100%')}}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginVertical: 10,
+        }}>
+        <Icon
+          name="arrow-back-outline"
+          size={30}
+          color="white"
+          onPress={() => navigation.pop()}
+        />
+        <Text
+          style={{
+            color: 'white',
+            marginLeft: 100,
+            fontWeight: '600',
+            fontSize: 16,
+          }}>
+          Select your country
+        </Text>
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+        }}>
+        <View style={{marginTop: 40, marginHorizontal: 10}}>
+          <TextInput
+            value={`${callingCode} ${countryName}`}
+            placeholder="select country..."
+            placeholderTextColor={'white'}
+            editable={false}
+            style={{
+              paddingVertical: 15,
+              backgroundColor: '#3A3B3C',
+              color: 'white',
+              fontSize: 16,
+              fontWeight: '600',
+              borderRadius: 8,
+            }}
+          />
+          <Icon
+            name="caret-down-outline"
+            size={35}
+            color="white"
+            style={{position: 'absolute', right: 10, top: 12}}
+            onPress={() => setVisible(true)}
+          />
+          <CountryPicker
+            {...{
+              withCallingCode,
+              withAlphaFilter,
+              withFilter,
+              countryChange,
+            }}
+            onSelect={country => SelectCountry(country)}
+            modalProps={{
+              visible: visible,
+            }}
+            onOpen={() => setVisible(true)}
+            onClose={() => setVisible(false)}
+          />
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#1877F2',
+            paddingVertical: 15,
+            alignItems: 'center',
+            marginHorizontal: 10,
+            borderRadius: 8,
+            marginTop: hp('68%'),
+          }}
+          onPress={nextPress}
+          disabled={countryName ? false : true}
+          >
+          <Text style={{color: 'white', fontSize: 18}}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+};
 
 export default SelectCountry;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
