58e4319289b2d225897cd295a0fda7de16df21a2
Udit tyagi <udittyagi455@gmail.com>
2024-03-07 21:57:29 +0530

    work on home component

--- src/components/BottomComponent/Home/index.tsx
+++ src/components/BottomComponent/Home/index.tsx
@@ -1,15 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {style} from './style';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Home = () => {
   return (
-    <KeyboardAwareScrollView>
-      <Text>Home</Text>
+    <KeyboardAwareScrollView style={style.keyboardView}>
+      <View style={style.header}>
+        <Text style={{color: 'white', fontSize: 22}}>Daily News</Text>
+        <Icon name="notifications-outline" size={30} color="white" />
+      </View>
     </KeyboardAwareScrollView>
-  )
-}
+  );
+};
 
-export default Home ;
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
